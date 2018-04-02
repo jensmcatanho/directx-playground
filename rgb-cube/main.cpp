d186373a4ac152a9026a2f201bf30766df5d414e
Jean Catanho <jensmcatanho@gmail.com>
2018-04-02 00:56:57 -0300

    rgb-cube: initial commit

---
+++ rgb-cube/main.cpp
@@ -0,0 +1,6 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	MessageBox(NULL, "", "RGB Cube", MB_ICONINFORMATION | MB_OK);
+	return 0;
+}
