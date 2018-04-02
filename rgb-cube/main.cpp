cfaed06c16f26a60c58b8462b32f65c145a92a63
Jean Catanho <jensmcatanho@gmail.com>
2018-04-02 01:55:14 -0300

    rgb-cube: add window creation

--- rgb-cube/main.cpp
+++ rgb-cube/main.cpp
@@ -1,6 +1,47 @@
 #include <windows.h>
+#include <windowsx.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
-	MessageBox(NULL, "", "RGB Cube", MB_ICONINFORMATION | MB_OK);
-	return 0;
+	HWND windowHandler;
+	WNDCLASSEX windowClass;
+	ZeroMemory(&windowClass, sizeof(WNDCLASSEX));
+
+	windowClass.cbSize = sizeof(WNDCLASSEX);
+	windowClass.style = CS_HREDRAW | CS_VREDRAW;
+	windowClass.lpfnWndProc = WindowProc;
+	windowClass.hInstance = hInstance;
+	windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	windowClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	windowClass.lpszClassName = "WindowClass1";
+	RegisterClassEx(&windowClass);
+
+	int width = 1024;
+	int height = 768;
+	int xPos = (GetSystemMetrics(SM_CXSCREEN) - width) / 2;
+	int yPos = (GetSystemMetrics(SM_CYSCREEN) - height) / 2;
+
+	windowHandler = CreateWindowEx(NULL, "WindowClass1", "RGB Cube", WS_OVERLAPPEDWINDOW, xPos, yPos, width, height, NULL, NULL, hInstance, NULL);
+	ShowWindow(windowHandler, nShowCmd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0)) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+	switch (message) {
+		case WM_DESTROY:
+			{
+				PostQuitMessage(0);
+				return 0;
+			} break;
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
